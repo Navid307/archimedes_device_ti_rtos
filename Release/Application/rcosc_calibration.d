ddc01f555eecc149d6842097076f0d747efd4d6b
Navid307 <navid.h307@gmail.com>
2023-06-08 14:42:15 -0400

    Deleted Release folder

--- Release/Application/rcosc_calibration.d
+++
@@ -1,4 +0,0 @@
-# FIXED
-
-Application/rcosc_calibration.o: \
- C:/ti/simplelink_cc13xx_cc26xx_sdk_6_41_00_17/source/ti/ble5stack/common/cc26xx/rcosc/rcosc_calibration.c
--- Release/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -Wl,-l"C:/ti/simplelink_cc13xx_cc26xx_sdk_6_41_00_17/examples/rtos/LP_CC1352P7_4/ble5stack/project_zero/tirtos7/ticlang/cc13x2x7_cc26x2x7_app_tirtos7.cmd" -Wl,-lti_utils_build_linker.cmd.genlibs -Wl,-l"ti/devices/cc13x2x7_cc26x2x7/driverlib/bin/ticlang/driverlib.lib" -Wl,-llibc.a
-
